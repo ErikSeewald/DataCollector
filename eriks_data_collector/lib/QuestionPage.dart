33da9dc96c496563531a904fba1a093e60e942bc
ErikSeewald <erik.seewaldx@gmail.com>
2023-10-05 11:23:36 +0200

    json

--- eriks_data_collector/lib/QuestionPage.dart
+++ eriks_data_collector/lib/QuestionPage.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+Future<List<Map<String, dynamic>>> loadQuestions() async {
+  String jsonString = await rootBundle.loadString('assets/questions/daily_questions.json');
+  List<dynamic> jsonData = jsonDecode(jsonString);
+  return jsonData.cast<Map<String, dynamic>>();
+}
 
 class QuestionPage extends StatelessWidget {
   @override
