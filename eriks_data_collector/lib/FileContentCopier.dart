833f2bfccef45f74c31dc1ef8b6ffa88d7450cad
Erik <erik.seewaldx@gmail.com>
2024-05-16 09:41:47 +0200

    update answer file fallback

--- eriks_data_collector/lib/FileContentCopier.dart
+++ eriks_data_collector/lib/FileContentCopier.dart
@@ -31,7 +31,12 @@ class FileContentCopier {
   static Future<File?> _getAnswerFile() async {
     try {
       Directory directory = await getApplicationDocumentsDirectory();
-      return File('${directory.path}/answers.json');
+
+      File file = File('${directory.path}/answers.json');
+      if (!await file.exists()) {
+        await file.writeAsString('{}');
+      }
+      return file;
     } catch (e) {
       return null;
     }
@@ -39,5 +44,27 @@ class FileContentCopier {
 
   static void _shareFile(File file) {
     Share.shareXFiles([XFile(file.path)], text: 'Your JSON file');
+  }
+
+  // Debug function for me to manually inject a change to previous
+  // answers back into my phone (requires an answers_edit.json file to be put
+  // into /assets/. The assets in pubspec.yaml also need to be updated)
+  static Future<void> _copyAssetToFile() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+
+    // Delete previous file
+    try {
+      File file = File('${directory.path}/answers.json');
+      if (await file.exists()) {
+        await file.delete();
+      }
+    } catch (e) {
+      print("Failed to delete {file}");
+      return;
+    }
+
+    String fileContents = await rootBundle.loadString('assets/answers_edit.json');
+    File file = File('${directory.path}/answers.json');
+    await file.writeAsString(fileContents);
   }
 }
